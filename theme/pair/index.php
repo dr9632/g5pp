298a4570d039a98d6eac8ce535942305a610f156
Han <39016276+dr9632@users.noreply.github.com>
2025-10-10 12:04:27 +0900

    이미지 출력 영역, 대사 출력 영역 설정

--- theme/pair/index.php
+++ theme/pair/index.php
@@ -10,15 +10,38 @@ if (G5_IS_MOBILE) {
 include_once(G5_THEME_PATH.'/head.php');
 ?>
 
-<h2 class="sound_only">최신글</h2>
-
-<div class="latest_wr">
-<!-- 최신글 시작 { -->
-    <?php
-    echo latest_all('theme/new_contents','전체게시판',10,24,array('notice'));
-    ?>
-    <!-- } 최신글 끝 -->
+<div class="main_visual">
+    <div class="char_layer">
+        <img src="<? echo G5_THEME_IMG_URL.'/1207(1).png' ?>"/>
+    </div> 
+    <div class="char_line">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed consequat lorem, sed eleifend elit. Mauris nisl nulla, iaculis id tristique sit amet, volutpat suscipit urna. Etiam egestas aliquet tincidunt. Curabitur ac venenatis velit. Praesent blandit id nulla ut ullamcorper. Suspendisse potenti. Quisque suscipit venenatis odio lacinia tempor. Suspendisse tincidunt arcu vitae magna pulvinar viverra ut at sem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus scelerisque et ipsum nec mollis. Fusce elit neque, aliquet ut tortor in, iaculis rhoncus leo. Mauris tempor lectus convallis purus commodo fringilla.</p>
+    </div>
 </div>
+<div class="main_contents">
+    <h2 class="sound_only">최신글</h2>
+    <div class="latest_wr">
+    <!-- 최신글 시작 { -->
+        <?php
+        echo latest_all('theme/new_contents','전체게시판',10,24,array('notice'));
+        ?>
+        <!-- } 최신글 끝 -->
+    </div>
+</div>
+
+<script>
+var i = 0;
+var txt = 'Lorem ipsum dummy text blabla.';
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+</script>
 
 <?php
 include_once(G5_THEME_PATH.'/tail.php');
